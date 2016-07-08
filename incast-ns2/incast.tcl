578c0fd7a72d9a3a8a32e56368059fc7d3c444fb
wei bai <baiwei0427@sina.com>
2016-07-09 03:41:24 +0800

    Add warmup and multi-iterations to incast

--- incast-ns2/incast.tcl
+++ incast-ns2/incast.tcl
@@ -23,6 +23,8 @@ TCP_pair instproc send { num_bytes } {
 
         $tcps set signal_on_empty_ TRUE
         $tcps advance-bytes $num_bytes
+
+        #puts "Run send"
 }
 
 TCP_pair instproc set_incast_callback { incast_group } {
@@ -37,6 +39,8 @@ Agent/TCP/FullTcp instproc set_incast_callback {incast_group} {
 
 Agent/TCP/FullTcp instproc done_data {} {
         $self instvar incast_ctrl
+
+        #puts "Run done_data"
 
         if { [info exists incast_ctrl] } {
                 $incast_ctrl fin_notify
@@ -61,28 +65,62 @@ Incast_group instproc add_pair { tcp_pair } {
         $tcp_pair set_incast_callback $self
 }
 
-Incast_group instproc send_per_pair { num_bytes } {
-        $self instvar fin_pairs num_pairs tcp_pairs
-        $self set fin_pairs 0
+set jitter_rng [new RNG]
+$jitter_rng seed 5251
 
+Incast_group instproc send_per_pair { jitter_period num_bytes } {
+        global ns jitter_rng
+        $self instvar fin_pairs num_pairs tcp_pairs start_time
+
+        $self set fin_pairs 0
+        $self set start_time [$ns now]
+
+        #puts "Run send_per_pair"
         for {set i 0} {$i < $num_pairs} {incr i} {
-                $tcp_pairs($i) send $num_bytes
+                set next_time [expr [$ns now] + [$jitter_rng uniform 0.0 $jitter_period]]
+                $ns at $next_time "$tcp_pairs($i) send $num_bytes"
         }
 }
 
 Incast_group instproc fin_notify {} {
-        global ns start_time bytes_per_sender
+        global ns start_time
 
-        $self instvar fin_pairs num_pairs
+        $self instvar fin_pairs num_pairs warmup_mode num_iter tot_num_bytes jitter_time start_time
         $self set fin_pairs [expr $fin_pairs + 1]
 
-        if {$fin_pairs == $num_pairs} {
-                set fin_time [$ns now]
-                set duration [expr $fin_time - $start_time]
-                set goodput [expr $bytes_per_sender * 8 * $num_pairs / $duration / 1000000000]
-                puts "Goodput $goodput Gbps"
-                exit 0
+        if { $fin_pairs == $num_pairs } {
+                set duration [expr [$ns now] - $start_time]
+                set goodput [expr [$self set tot_num_bytes] * 8 / $duration / 1000000000]
+
+                if { $warmup_mode != 1} {
+                        puts "($num_iter) Time $duration seconds Goodput $goodput Gbps"
+                        $self set num_iter [expr $num_iter - 1]
+
+                        if { $num_iter > 0} {
+                                $self send_per_pair $jitter_time [expr $tot_num_bytes / $num_pairs]
+                        } else {
+                                exit 0
+                        }
+                }
         }
+}
+
+Incast_group instproc warmup { jitter_period num_bytes } {
+        $self instvar warmup_mode tot_num_bytes num_pairs
+        $self set warmup_mode 1
+        $self set tot_num_bytes [expr $num_pairs * $num_bytes]; # total size in bytes
+
+        $self send_per_pair $jitter_period $num_bytes
+}
+
+Incast_group instproc schedule { jitter_period num_bytes iter } {
+        $self instvar warmup_mode jitter_time num_iter num_pairs tot_num_bytes
+        $self set warmup_mode 0
+        $self set num_iter $iter; # iterations
+        $self set tot_num_bytes [expr $num_pairs * $num_bytes]; # total size in bytes
+        $self set jitter_time $jitter_period
+
+        $self send_per_pair $jitter_period $num_bytes
 }
 
 set num_sender [lindex $argv 0] ; # number of senders
@@ -104,6 +142,7 @@ Agent/TCP set minrto_ $rto_min
 Agent/TCP set rtxcur_init_ $rto_min ; # initial RTO
 Agent/TCP set windowOption_ 0
 
+Agent/TCP/FullTcp set nodelay_ true; # disable Nagle
 Agent/TCP/FullTcp set segsize_ $packet_size
 Agent/TCP/FullTcp set segsperack_ 1 ; # ACK frequency
 Agent/TCP/FullTcp set interval_ 0.000006 ; #delayed ACK interval
@@ -126,6 +165,6 @@ for {set i 0} {$i < $num_sender} {incr i} {
         $incast add_pair $tcp_pairs($i)
 }
 
-set start_time 0.1
-$ns at $start_time "$incast send_per_pair $bytes_per_sender"
+$ns at 0.1 "$incast warmup 0.00000 100"
+$ns at 1 "$incast schedule 0.00000 $bytes_per_sender 10"
 $ns run
