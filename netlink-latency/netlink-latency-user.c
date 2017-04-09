db030a3e6c11e10f6d874f03bbd39834335048c4
wei bai <baiwei0427@sina.com>
2017-04-09 21:26:33 +0800

    Update netlink latency measurement program
    memset msg is necessary

--- netlink-latency/netlink-latency-user.c
+++ netlink-latency/netlink-latency-user.c
@@ -8,25 +8,27 @@
 #define NETLINK_USER 31
 #define MAX_PAYLOAD 1024 /* maximum payload size*/
 
-struct sockaddr_nl src_addr, dest_addr;
-struct nlmsghdr *nlh = NULL;
-struct iovec iov;
-int sock_fd;
-struct msghdr msg;
-struct timespec time_start={0, 0},time_end={0, 0};
-
 int main()
 {
+        struct sockaddr_nl src_addr, dest_addr;
+        struct nlmsghdr *nlh = NULL;
+        struct iovec iov;
+        int sock_fd;
+        struct msghdr msg;
+        struct timespec time_start = {0, 0}, time_end = {0, 0};
+
+        memset(&src_addr, 0, sizeof(src_addr));
+        memset(&dest_addr, 0, sizeof(dest_addr));
+        memset(&msg, 0, sizeof(msg));
+
         if ((sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER)) < 0) {
                 return -1;
         }
 
-        memset(&src_addr, 0, sizeof(src_addr));
         src_addr.nl_family = AF_NETLINK;
         src_addr.nl_pid = getpid();     /* self pid */
         bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
 
-        memset(&dest_addr, 0, sizeof(dest_addr));
         dest_addr.nl_family = AF_NETLINK;
         dest_addr.nl_pid = 0;   /* For Linux Kernel */
         dest_addr.nl_groups = 0;        /* unicast */
