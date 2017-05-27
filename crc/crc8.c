a3ce1df45b72d3bbd84142547bbfc232b64ee68f
wei bai <baiwei0427@sina.com>
2017-05-28 02:04:07 +0800

    Add crc8 lookup table based implementation

--- crc/crc8.c
+++ crc/crc8.c
@@ -1,19 +1,54 @@
 #include <stdio.h>
 
+unsigned char crc_table[256];
+const unsigned char generator = 0x1d;   /* generator polynomial for CRC 8 */
+
+void gen_lookuptable()
+{
+        unsigned i, j;
+        unsigned char crc, msb;
+
+        for (i = 0; i < 256; i++) {
+                crc = i;
+                for (j = 0; j < 8; j ++) {
+                        msb = crc & 0x80;
+                        crc = crc << 1;
+                        if (msb) {
+                                crc = crc ^ generator;
+                        }
+                }
+                crc_table[i] = crc;
+        }
+}
+
+/* Lookup table based solution */
+unsigned char crc8_lookuptable(unsigned char *data, unsigned int len)
+{
+        unsigned char crc = 0;
+        unsigned int i;
+
+        for (i = 0; i < len; i++) {
+                crc = crc ^ data[i];    /* XOR-in next input byte */
+                crc = crc_table[(int)crc];      /* get CRC result based on lookup table */
+        }
+
+        return crc;
+}
+
+/* General CRC-8 bitwise implementation */
 unsigned char crc8_simple(unsigned char *data, unsigned int len)
 {
-        const unsigned char generator = 0x1d; /* generator polynomial for CRC 8 */
         unsigned char crc = 0, msb;
         unsigned int i, j;
 
         for (i = 0; i < len; i++) {
-                crc = (crc ^ data[i]);
+                crc = crc ^ data[i];    /* XOR-in next input byte */
                 for (j = 0; j < 8; j++) {
                         msb = crc & 0x80;
-                        crc = (crc << 1);       /* left shift */
+                        crc = crc << 1; /* left shift */
 
                         if (msb) {      /* XOR crc if MSB == 1 */
-                                crc = (crc ^ generator);
+                                crc = crc ^ generator;
                         }
                 }
         }
@@ -37,6 +72,10 @@ int main(int argc, char **argv)
         data[2] = (input & 0x0000ff00) >> 8;
         data[3] = input & 0x000000ff;
 
-        printf("CRC8 value of %d (0x%.2x%.2x%.2x%.2x) = %d\n", input, data[0], data[1], data[2], data[3], crc8_simple(data, 4));
+        gen_lookuptable();      /* generate lookup table */
+
+        printf("CRC8 (bitwise) of %d (0x%.2x%.2x%.2x%.2x) = %d\n", input, data[0], data[1], data[2], data[3], crc8_simple(data, 4));
+        printf("CRC8 (lookup table) of %d (0x%.2x%.2x%.2x%.2x) = %d\n", input, data[0], data[1], data[2], data[3], crc8_lookuptable(data, 4));
+
         return 0;
 }
