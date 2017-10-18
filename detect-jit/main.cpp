fef9efb81a8534cc912680ac44411894b115a750
Wei Bai <baiwei0427@users.noreply.github.com>
2017-10-18 11:25:46 +0800

    Update
    After reading http://www.pcre.org/original/doc/html/pcre_config.html, I eventually know how to user pcre_config.

--- detect-jit/main.cpp
+++ detect-jit/main.cpp
@@ -4,16 +4,20 @@ using namespace std;
 
 int main()
 {
-        char info[100];
-        int result = pcre_config(PCRE_CONFIG_JIT, (void*)info);
-        
-        if (result == 1) {
-                cout << "Support JIT" << endl;
-        } else if (result == 0) {
-                cout << "Do not support JIT" << endl;
-        } else {
-                cout << "Unknown errors" << endl;
-        }
+        int support_jit;
 
+        // check availability of just-in-time (JIT) compiler support
+        if (pcre_config(PCRE_CONFIG_JIT, (void*)&support_jit) == 0) {
+                if (support_jit == 1) {
+                        cout << "Support JIT" << endl;        
+                } else if (support_jit ==0) {
+                        cout << "Do not support JIT" << endl;
+                } else {
+                        cout << "Unknown result " << support_jit << endl;
+                }
+        } else {
+                cout << "Error with PCRE_CONFIG_JIT" << endl;
+        }
+        
         return 0;
 }
