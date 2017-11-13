304938323d624071995044eb86ae05ebc70071ad
Wei Bai <baiwei0427@users.noreply.github.com>
2017-11-13 20:19:04 +0800

    ‘>>’ should be ‘> >’ within a nested template argument list

--- map-vector/main.cpp
+++ map-vector/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    map<int, vector<int>> result;
+    map<int, vector<int> > result;
     int key_start = 1, key_end = 5;
     
     for (int key = key_start; key <= key_end; key++) {
@@ -15,10 +15,10 @@ int main()
             vals.push_back(val);
         }
         
-        result.insert(pair<int, vector<int>>(key, vals));
+        result.insert(pair<int, vector<int> >(key, vals));
     }
     
-    map<int, vector<int>>::iterator itr;
+    map<int, vector<int> >::iterator itr;
     for (itr = result.begin(); itr != result.end(); itr++) {
         cout << itr->first << " : ";
         for (int i = 0; i < itr->second.size(); i++) {
