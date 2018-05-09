b6d7b2b9ad5fff2c5eabe594e40e4b94cf824257
Wei Bai <baiwei0427@users.noreply.github.com>
2018-05-09 23:15:23 +0800

    Retrieve all submatches

--- re2demo/main.cpp
+++ re2demo/main.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
 #include <string>
-#include "re2.h"
+#include <vector>
+#include <re2/re2.h>
+#include <re2/stringpiece.h>
 
 using namespace std;
 
-
 int main(int argc, char **argv)
 {       
-        /* We require two arguments: pattern, subject string. */
-        if (argc != 3) {
-                cerr << argv[0] << " [regex pattern] [subject string]" << endl;
-                return -1;
-        }
+        // We require two arguments: pattern, subject string. 
+	if (argc != 3) {
+		cerr << argv[0] << " [regex pattern] [subject string]" << endl;
+  		return -1;
+  	}
 
-        int 
-        string s;
+        unsigned int count = 10;
+        re2::StringPiece submatches[count];
+        RE2::Arg args[count];
+        RE2::Arg* args_ptrs[count];
         RE2 re("(" + string(argv[1]) + ")");
-        
+
         if (!re.ok()) {
                 cerr << "Fail to compile regex pattern " << argv[1] << endl;
         }
-        
-        if (RE2::PartialMatch(argv[2], re, &s)) {
+
+        // Get # of groups in the regex pattern 
+        unsigned int args_count = re.NumberOfCapturingGroups();
+
+        if (args_count > count) {
+                args_count = count;
+        }
+
+        for (unsigned int i = 0; i < args_count; i++) {
+                args[i] = &submatches[i];
+                args_ptrs[i] = &args[i];
+        }
+
+        if (RE2::PartialMatchN(re2::StringPiece(argv[2]), re, args_ptrs, args_count)) {
                 cout << "Match" << endl;
-                cout << s << endl;
+                for (unsigned int i = 0; i < args_count; i++) {
+                        printf("offset %d length %d: ", submatches[i].data() - argv[2], submatches[i].length());
+                        printf("%.*s\n", submatches[i].length(), submatches[i].data());
+                }
+
         } else {
                 cout << "Don't match" << endl;
         }
