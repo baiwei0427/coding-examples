1950b2693ce047d64960f039243fede4a737a02f
Wei Bai <baiwei0427@gmail.com>
2023-06-11 01:48:09 -0700

    Update rdma-monitor.c

--- rdma-monitor/rdma-monitor.c
+++ rdma-monitor/rdma-monitor.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <dlfcn.h>
-#include <infiniband/verbs.h>
 
 static void *ibv_handle = NULL;
 static int (*real_ibv_modify_qp)(struct ibv_qp *qp, struct ibv_qp_attr *attr, int mask) = NULL;
+static struct ibv_context* (*real_ibv_open_device)(struct ibv_device *device) = NULL;
 
 int ibv_modify_qp(struct ibv_qp *qp, struct ibv_qp_attr *attr, int mask) {
     if (!ibv_handle) {
@@ -30,6 +30,34 @@ int ibv_modify_qp(struct ibv_qp *qp, struct ibv_qp_attr *attr, int mask) {
     printf("ibv_modify_qp intercepted\n");
     int ret = real_ibv_modify_qp(qp, attr, mask);
     printf("ibv_modify_qp returns %d\n", ret);
+
+    return ret;
+}
+
+struct ibv_context *ibv_open_device(struct ibv_device *device)
+{
+    if (!ibv_handle) {
+        ibv_handle = dlopen("libibverbs.so.1", RTLD_NOW);
+        if (!ibv_handle) {
+            printf("libibverbs.so.1 not found\n");
+            return NULL;
+        } else {
+            printf("libibverbs.so.1 found\n");
+        }
+    }
+
+    if (!real_ibv_open_device) {
+        real_ibv_open_device = dlsym(ibv_handle, "ibv_open_device");
+
+        if (!real_ibv_open_device) {
+            printf("ibv_open_device not found\n");
+            return NULL;
+        }
+    }
+
+    printf("ibv_open_device intercepted\n");
+    struct ibv_context *ret = real_ibv_open_device(device);
+    printf("ibv_open_device returns %p\n", ret);
 
     return ret;
 }
